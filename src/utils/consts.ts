f480261f00dea429d7d98ca2435435de56390e0d
Edem Veliev <edem010100@gmail.com>
2026-01-18 15:46:00 +0500

    ci/cd

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,4 +1,8 @@
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "https://api.amis.uz";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
+
+console.log(BASE_URL);
 
 export const UserRoles = {
     superadmin: 'superadmin',
